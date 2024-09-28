84f03b854defdad968caf7048c7abcdc5ccf238c
Verdessence <mrflooringsman@gmail.com>
2024-09-29 00:07:50 +0800

    add my time!

--- js/common.js
+++ js/common.js
@@ -22,6 +22,7 @@ function headers() {
 }
 
 headers();
+
 /*
     <head>
         <link rel="stylesheet" href="css/styles.css">
@@ -31,3 +32,52 @@ headers();
         <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
     </head>
 */
+
+function time(eventId, startTimeString, endTimeString, timeZone, note) {
+    const startTime = new Date(`${startTimeString}${timeZone}`);
+    const endTime = new Date(`${endTimeString}${timeZone}`);
+
+    const optionsDate = {
+         year: 'numeric', month: 'long', weekday: 'long', day: 'numeric',
+    };
+    const optionsTime = {
+        hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'long',    
+    };
+
+    const localTimeString = startTime.toLocaleString(undefined, optionsDate);
+    const localStartTimeString = startTime.toLocaleString(undefined, optionsTime);
+    const localEndTimeString = endTime.toLocaleString(undefined, optionsTime);
+
+    const eventElement = document.createElement('div');
+    eventElement.innerHTML = `
+    <p>${localTimeString} from ${localStartTimeString} - ${localEndTimeString}</p><p>Notes: ${note}</p>
+    `;
+    document.getElementById(eventId).appendChild(eventElement);
+}
+
+function timedates(events) {
+    events.forEach(element => {
+        time('event-list', element.startTimeString, element.endTimeString, element.timeZone, element.note);
+    });
+}
+
+const events = [
+    {
+        startTimeString: "2024-11-01T08:00:00",
+        endTimeString: "2024-11-01T23:59:59",
+        timeZone: "+08:00",
+        note: "Maybe on a Catholic School, but IDK"
+    },
+    {
+        startTimeString: "2024-11-27T08:00:00",
+        endTimeString: "2024-11-27T14:00:00",
+        timeZone: "+08:00",
+        note: "Same Applies until September 30 Philippine Time"
+    },
+    {
+    startTimeString: "2024-12-23T08:00:00",
+    endTimeString: "2025-01-01T00:00:00",
+    timeZone: "+08:00",
+    note: ""
+    }
+];
